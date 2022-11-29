e564adc40a4e030a6e0c71ef13700185416f1749
Simion <simion.okola@student.moringaschool.com>
2022-11-30 02:44:56 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  post '/signup',to: 'users#create'
+  get '/me',to: 'users#show'
+  delete '/logout',to: 'sessions#destroy'
+  post '/login',to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
